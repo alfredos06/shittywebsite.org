5e41f2cbcf72b56f8b4b395efb40eb194b078420
alfredos06 <famdahlin4@gmail.com>
2026-02-26 18:08:58 +0100

    added egul

---
+++ egul/egul.js
@@ -0,0 +1,6 @@
+window.setTimeout(function(){
+
+        // Move to a new location or you can do something else
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley";
+
+    }, 5000);
